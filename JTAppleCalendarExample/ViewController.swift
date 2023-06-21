57729e1201544e6a098d1150411899de22572383
yanghojoon <yanghojoon8487@gmail.com>
2023-06-21 15:44:10 +0900

    feat: 기본 달력으로 넘어갈 수 있는 버튼 추가

--- JTAppleCalendarExample/ViewController.swift
+++ JTAppleCalendarExample/ViewController.swift
@@ -7,12 +7,49 @@
 
 import UIKit
 
+import SnapKit
+
 final class ViewController: UIViewController {
+    private let basicCalendarButton: UIButton = {
+        let button = UIButton()
+        
+        var buttonConfiguration = UIButton.Configuration.bordered()
+        
+        buttonConfiguration.background.backgroundColor = .systemGray6
+        buttonConfiguration.baseForegroundColor = .black
+        
+        buttonConfiguration.title = "기본 달력"
+        
+        let handler: UIButton.ConfigurationUpdateHandler = { button in
+            switch button.state {
+            case .highlighted:
+                button.configuration?.background.backgroundColor = .systemGray4
+            default:
+                button.configuration?.background.backgroundColor = .systemGray6
+            }
+        }
+
+        button.configuration = buttonConfiguration
+        button.configurationUpdateHandler = handler
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        render()
     }
 
+    private func render() {
+        view.addSubview(basicCalendarButton)
 
+        basicCalendarButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(150)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(100)
+            $0.height.equalTo(50)
+        }
+    }
 }
 
