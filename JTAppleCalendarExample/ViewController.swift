3e85838b6d6c4ae6a25a82274a8396263b3d0a86
yanghojoon <yanghojoon8487@gmail.com>
2023-06-23 11:09:14 +0900

    feat: 연습장 버튼 및 화면 구현

--- JTAppleCalendarExample/ViewController.swift
+++ JTAppleCalendarExample/ViewController.swift
@@ -34,6 +34,31 @@ final class ViewController: UIViewController {
         
         return button
     }()
+    
+    private let otherButton: UIButton = {
+        let button = UIButton()
+        
+        var buttonConfiguration = UIButton.Configuration.bordered()
+        
+        buttonConfiguration.background.backgroundColor = .systemGray6
+        buttonConfiguration.baseForegroundColor = .black
+        
+        buttonConfiguration.title = "연습장"
+        
+        let handler: UIButton.ConfigurationUpdateHandler = { button in
+            switch button.state {
+            case .highlighted:
+                button.configuration?.background.backgroundColor = .systemGray4
+            default:
+                button.configuration?.background.backgroundColor = .systemGray6
+            }
+        }
+
+        button.configuration = buttonConfiguration
+        button.configurationUpdateHandler = handler
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,9 +69,17 @@ final class ViewController: UIViewController {
 
     private func render() {
         view.addSubview(basicCalendarButton)
+        view.addSubview(otherButton)
 
         basicCalendarButton.snp.makeConstraints {
             $0.top.equalToSuperview().inset(150)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(100)
+            $0.height.equalTo(50)
+        }
+        
+        otherButton.snp.makeConstraints {
+            $0.top.equalTo(basicCalendarButton.snp.bottom).offset(50)
             $0.centerX.equalToSuperview()
             $0.width.equalTo(100)
             $0.height.equalTo(50)
@@ -55,6 +88,7 @@ final class ViewController: UIViewController {
 
     private func setAttributes() {
         basicCalendarButton.addTarget(self, action: #selector(tapBasicCalendarButton), for: .touchUpInside)
+        otherButton.addTarget(self, action: #selector(tapOtherButton), for: .touchUpInside)
     }
 
     @objc
@@ -62,6 +96,13 @@ final class ViewController: UIViewController {
         let basicCalendarViewController = BasicCalendarViewController()
 
         navigationController?.pushViewController(basicCalendarViewController, animated: true)
+    }
+    
+    @objc
+    private func tapOtherButton() {
+        let practiceViewController = PracticeViewController()
+        
+        navigationController?.pushViewController(practiceViewController, animated: true)
     }
 }
 
